41fce7757064f12bb72f06581fec1eadc1c30fb2
Adam Gayoso <adamgayoso@users.noreply.github.com>
2022-10-02 22:43:21 -0700

    initial docs (#4)
    * initial docs
    
    * update docs
    
    * update docs
    
    * update docs
    
    * update docs
    
    * update docs
    
    * update docs

--- src/scib_metrics/utils/_silhouette.py
+++ src/scib_metrics/utils/_silhouette.py
@@ -93,12 +93,27 @@ def _euclidean_distance(x: np.array, y: np.array) -> float:
 
 
 @jax.jit
-def cdist(x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    """Jax implementation of scipy.spatial.distance.cdist."""
+def cdist(x: np.ndarray, y: np.ndarray) -> jnp.ndarray:
+    """Jax implementation of :func:`scipy.spatial.distance.cdist`.
+
+    Uses euclidean distance.
+
+    Parameters
+    ----------
+    x
+        Array of shape (n_samples_a, n_features)
+    y
+        Array of shape (n_samples_b, n_features)
+
+    Returns
+    -------
+    dist: jnp.ndarray
+        Array of shape (n_samples_a, n_samples_b)
+    """
     return jax.vmap(lambda x1: jax.vmap(lambda y1: _euclidean_distance(x1, y1))(y))(x)
 
 
-def silhouette_samples(X: np.ndarray, labels: np.ndarray):
+def silhouette_samples(X: np.ndarray, labels: np.ndarray) -> np.ndarray:
     """Compute the Silhouette Coefficient for each observation.
 
     Code inspired by:
@@ -106,14 +121,17 @@ def silhouette_samples(X: np.ndarray, labels: np.ndarray):
 
     Parameters
     ----------
-    X : array-like of shape (n_samples_a, n_features) representing an
-        array of pairwise distances between samples, or a feature array.
-    labels : array-like of shape (n_samples,) representing label values
+    X
+        Array of shape (n_samples, n_features) representing a
+        feature array.
+    labels
+        Array of shape (n_samples,) representing label values
         for each observation.
 
     Returns
     -------
-    silhouette : array-like of shape (n_samples,)
+    silhouette: np.ndarray
+        Array of shape (n_samples,)
         Silhouette Coefficients for each sample.
     """
     if X.shape[0] != labels.shape[0]:

1f67006ede97fc1189abbbef09ae9166e56edc72
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-02 21:16:30 -0700

    add silhouette

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,7 @@
-import pytest
+import jax.numpy as jnp
+import numpy as np
+from scipy.spatial.distance import cdist as sp_cdist
+from sklearn.metrics import silhouette_samples as sk_silhouette_samples
 
 import scib_metrics
 
@@ -7,6 +10,13 @@ def test_package_has_version():
     scib_metrics.__version__
 
 
-@pytest.mark.skip(reason="This decorator should be removed when test passes.")
-def test_example():
-    assert 1 == 0  # This test is designed to fail.
+def test_cdist():
+    x = jnp.array([[1, 2], [3, 4]])
+    y = jnp.array([[5, 6], [7, 8]])
+    assert np.allclose(scib_metrics.silhouette.cdist(x, y), sp_cdist(x, y))
+
+
+def test_silhouette_samples():
+    X = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+    labels = np.array([0, 0, 1, 1])
+    assert np.allclose(scib_metrics.silhouette.silhouette_samples(X, labels), sk_silhouette_samples(X, labels))

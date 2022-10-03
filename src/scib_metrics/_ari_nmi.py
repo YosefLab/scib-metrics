96194b649c7bf0c1feea43c845c29de62b672d84
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-03 09:20:32 -0700

    docs

--- src/scib_metrics/_ari_nmi.py
+++ src/scib_metrics/_ari_nmi.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import numpy as np
 from sklearn.metrics.cluster import adjusted_rand_score, normalized_mutual_info_score
 
@@ -10,7 +12,7 @@ def _compute_clustering(X: np.ndarray, n_clusters: int) -> np.ndarray:
     return kmeans.labels_
 
 
-def nmi_ari_cluster_labels(X: np.ndarray, labels: np.ndarray) -> float:
+def nmi_ari_cluster_labels(X: np.ndarray, labels: np.ndarray) -> Tuple[float, float]:
     """Compute nmi and ari between k-means clusters and labels.
 
     This deviates from the original implementation in scib by using k-means

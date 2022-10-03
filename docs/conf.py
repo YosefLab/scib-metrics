3c3893bcdbb208a4c12e29ca009d12138abd2008
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-02 22:33:38 -0700

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -102,7 +102,8 @@ pygments_style = "sphinx"
 nitpick_ignore = [
     # If building the documentation fails because of a missing link that is outside your control,
     # you can add an exception to this list.
-    #     ("py:class", "igraph.Graph"),
+    ("py:class", "np.ndarray"),
+    ("py:class", "jnp.ndarray"),
 ]
 
 

e37ca489ff78e907b84b9222412f70b189daf953
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-02 22:01:48 -0700

    update docs

--- src/scib_metrics/utils/_silhouette.py
+++ src/scib_metrics/utils/_silhouette.py
@@ -93,8 +93,23 @@ def _euclidean_distance(x: np.array, y: np.array) -> float:
 
 
 @jax.jit
-def cdist(x: np.ndarray, y: np.ndarray) -> np.ndarray:
-    """Jax implementation of scipy.spatial.distance.cdist."""
+def cdist(x: np.ndarray, y: np.ndarray) -> jnp.ndarray:
+    """Jax implementation of scipy.spatial.distance.cdist.
+
+    Uses euclidean distance.
+
+    Parameters
+    ----------
+    x
+        Array of shape (n_samples_a, n_features)
+    y
+        Array of shape (n_samples_b, n_features)
+
+    Returns
+    -------
+    dist
+        Array of shape (n_samples_a, n_samples_b)
+    """
     return jax.vmap(lambda x1: jax.vmap(lambda y1: _euclidean_distance(x1, y1))(y))(x)
 
 

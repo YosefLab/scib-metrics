36bd49b3968015c99fcd0602b696a328b034f671
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-04 08:58:42 -0700

    html options

--- docs/conf.py
+++ docs/conf.py
@@ -97,6 +97,10 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
 
+html_theme_options = {
+    "repository_url": "https://github.com/yoseflab/scib-metrics",
+    "use_repository_button": True,
+}
 
 pygments_style = "default"
 pygments_dark_style = "native"

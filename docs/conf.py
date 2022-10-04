845fc8c90d6c8a2e370863bf894b81dc50098c59
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-04 08:59:04 -0700

    html title

--- docs/conf.py
+++ docs/conf.py
@@ -96,6 +96,7 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
 #
 html_theme = "sphinx_book_theme"
 html_static_path = ["_static"]
+html_title = "scib-metrics"
 
 html_theme_options = {
     "repository_url": "https://github.com/yoseflab/scib-metrics",

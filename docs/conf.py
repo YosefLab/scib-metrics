4e26f44b79f650396ee6a8290c28e742cddb666c
adamgayoso <adamgayoso@users.noreply.github.com>
2022-10-09 10:48:48 -0700

    typehint defaults

--- docs/conf.py
+++ docs/conf.py
@@ -76,6 +76,7 @@ myst_url_schemes = ("http", "https", "mailto")
 nb_output_stderr = "remove"
 nb_execution_mode = "off"
 nb_merge_streams = True
+typehints_defaults = "braces"
 
 source_suffix = {
     ".rst": "restructuredtext",

a9612a9a2e41aad57a9e5482ca2470f72c8ac0d9
adamgayoso <adamgayoso@users.noreply.github.com>
2022-12-08 17:12:32 -0800

    fix math

--- src/scib_metrics/_graph_connectivity.py
+++ src/scib_metrics/_graph_connectivity.py
@@ -5,15 +5,7 @@ from scipy.sparse.csgraph import connected_components
 
 
 def graph_connectivity(X: csr_matrix, labels: np.ndarray) -> float:
-    r"""Quantify the connectivity of the subgraph per cell type label.
-
-    The final score is the average for all cell type labels :math:`C`, according to the equation:
-
-    .. math::
-        GC = \frac{1}{|C|}\sum_{c \in C}\frac{|{LCC(subgraph_c)}|}{|c|}
-
-    where :math:`|LCC(subgraph_c)|` stands for all cells in the largest connected component and :math:`|c|` stands for all cells of
-    cell type :math:`c`.
+    """Quantify the connectivity of the subgraph per cell type label.
 
     Parameters
     ----------

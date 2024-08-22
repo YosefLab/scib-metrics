4c7fe7be8de25f645dfdf91d3fef12e1f0d28949
Adam Gayoso <adamgayoso@users.noreply.github.com>
2024-08-22 08:06:27 +0100

    Update test_metrics.py

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -120,13 +120,6 @@ def test_nmi_ari_cluster_labels_leiden_reproducibility():
     assert nmi1 == nmi2
     assert ari1 == ari2
 
-    out1 = scib_metrics.nmi_ari_cluster_labels_leiden(X, labels, optimize_resolution=False, resolution=3.0, seed=1)
-    out2 = scib_metrics.nmi_ari_cluster_labels_leiden(X, labels, optimize_resolution=False, resolution=3.0, seed=100)
-    nmi1, ari1 = out1["nmi"], out1["ari"]
-    nmi2, ari2 = out2["nmi"], out2["ari"]
-    assert nmi1 != nmi2
-    assert ari1 != ari2
-
 
 def test_leiden_graph_construction():
     X, _ = dummy_x_labels(symmetric_positive=True, x_is_neighbors_results=True)

fd61be8889ca5af8c32b053bed3e5472c3346b56
Adam Gayoso <adamgayoso@users.noreply.github.com>
2024-11-03 21:55:07 +0000

    Update _core.py

--- src/scib_metrics/benchmark/_core.py
+++ src/scib_metrics/benchmark/_core.py
@@ -157,6 +157,10 @@ class Benchmarker:
         self._n_jobs = n_jobs
         self._progress_bar = progress_bar
 
+        if self._bio_conservation_metrics is None and self._batch_correction_metrics is None:
+            raise ValueError("Either batch or bio metrics must be defined.")
+
+        
         self._metric_collection_dict = {}
         if self._bio_conservation_metrics is not None:
             self._metric_collection_dict.update({"Bio conservation": self._bio_conservation_metrics})
